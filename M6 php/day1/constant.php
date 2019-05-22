40df3ab03a6ad0ebb117bfed2847514066765180
tranhiep <tranquanghiep222@gmail.com>
2019-05-22 19:02:20 +0700

    init

---
+++ M6 php/day1/constant.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    hằng số dùng để chứa thông tin
+    khác với biến trong quá trình chạy thì biến sẽ thay đổi giá trị
+    còn hằng số thì không thay đổi giá trị
+    khai báo hằng constant:def fine("tên hằng số", "giá trị của hằng số ");
+    quy tắc khai báo tên hằng số
+    khác với biến thì cần $ bắt đầu
+    hằng số chỉ nên chứa chữ svaf số
+    hằng số chỉ nên viết hoa
+
+</pre>
+<?php
+define("USERNAME", "nguyễn tuấn anh");
+define("USERID","123456789");
+echo "<br>".USERNAME;
+echo "<br>".USERID;
+?>
+
+
+
+
+</body>
+</html>
---
+++ M6 php/day1/datatype.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+ <pre>
+        Các kiểu dữ liệu trong php :
+        kiểu số nguyên int
+        kiểu số thực float
+        kiểu chuỗi string
+        kiểu null
+        kiểu boolean ( true or false )
+        kiểu đối tượng
+        kiểu mảng
+    </pre>
+ <h1>Kiểu số nguyên</h1>
+<?php $a=5;?>
+<h1>kiểu số thực </h1>
+<?php $b=1.5;?>
+<h1>kiểu chuỗi</h1>
+ <?php $c="chào php";?>
+<h1>kiểu boolean</h1>
+<?php $d1=$d=$e=false;?>
+ <?php $f1=$f=$g=true;?>
+
+ <h1>kiểu mảng</h1>
+<?php $mang=array("a","b","c","d");?>
+ <h1>kiểu null</h1>
+<?php $n=null;?>
+
+
+
+</body>
+</html>
